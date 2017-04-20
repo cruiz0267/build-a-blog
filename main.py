4c0b797ec2e79d83162cba3b415cb47128f691b0
CHRIS <cruiz0267@gmail.com>
2017-04-20 17:21:34 -0500

    completed project

--- main.py
+++ main.py
@@ -1,33 +1,77 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
 import jinja2
-import os
-
 from google.appengine.ext import db
 
-template_dir = os.path.join(os.path.dirname(__file__), "templates")
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 
-class MainHandler(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Blogpost(db.Model):
+    title = db.StringProperty(required = True)
+    blogpost = db.TextProperty( required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+
+class Blog(Handler):
+    def render_front(self, title="", blogpost = ""):
+        blogposts = db.GqlQuery("SELECT * FROM Blogpost "
+                                "ORDER BY created DESC LIMIT 5")
+
+        self.render("front.html", title=title, blogpost=blogpost, blogposts=blogposts)
+
     def get(self):
-        self.render('front.html')
+        self.render_front()
+
+class Newpost(Handler):
+    def render_newpost(self, title="", blogpost = "", error=""):
+        self.render("newpost.html", title=title, blogpost=blogpost, error=error)
+
+    def get(self):
+        self.render_newpost()
+
+    def post(self):
+        title = self.request.get("title")
+        blogpost = self.request.get("blogpost")
+
+        if title and blogpost:
+            b = Blogpost(title = title, blogpost=blogpost)
+            b.put()
+
+            id = b.key().id()
+            self.redirect("/blog/%s" % id)
+        else:
+            error = "Your post needs a title and post"
+            self.render_newpost(title, blogpost, error)
+
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id):
+        blogpost = Blogpost.get_by_id(int(id))
+
+        if blogpost:
+            t = jinja_env.get_template("singlepost.html")
+            response = t.render(blogpost=blogpost)
+        else:
+            error = "There is no blogpost with that id"
+            t = jinja_env.get_template("error.html")
+            response = t.render(error=error)
+
+        self.response.out.write(response)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/blog', Blog),
+    ('/newpost', Newpost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
